204ec3efd392f26c78be566bbc058649e88dc889
apps221 <maggie.a.hong@gmail.com>
2025-02-25 08:38:37 -0500

    nav bar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, on
 function App() {
   const [user, setUser] = React.useState({});
   const [isLoading, setIsLoading] = React.useState(true);
+  const [isLoggedin, setIsLoggedin] = React. useState(false)
   React.useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       setIsLoading(false)
@@ -31,6 +32,7 @@ signInWithEmailAndPassword(auth, 'email@email.com','test1234')
 .then((data) => {
   console.log(data.user)
   setUser(data.user);
+  setIsLoggedin(true)
   // ...
 })
 .catch((error) => {
@@ -41,13 +43,26 @@ console.log(error);
     signOut(auth);
     setUser({});
   }
+  function loggedIn () {
+    if (isLoggedin) {
+      return ( 
+        <>
+        <button onClick={logout}>{user.email[0].toUpperCase()}</button>
+        </>
+      )
+    }
+    else return (
+      <>
+       <button onClick={register}>Register</button>
+       <button onClick={login}>Login</button>
+      </>
+    )
+    
+  }
   return (
     <div className="App">
-     <button onClick={register}>Register</button>
-     <button onClick={login}>Login</button>
-     <button onClick={logout}>Logout</button>
-     {isLoading ? "loading" : user.email}
-    
+    {isLoading ? "loading" : <>  <button onClick={logout}>{user.email[0].toUpperCase()}</button></>}
+    {loggedIn()}
     </div>
   );
 }
